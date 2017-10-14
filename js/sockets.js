fea4c1859efc425a99f7771847ac0eb00e04e103
UnicodingUnicorn <7555ic@gmail.com>
2017-10-14 21:35:15 +0800

    Testing timing

---
+++ js/sockets.js
@@ -0,0 +1,32 @@
+//var SOCKET_HOST = "192.168.99.100";
+var SOCKET_HOST = "172.22.152.16";
+var SOCKET_PORT = "10202";
+
+var timediff = 0;
+
+var options = {
+  hostname: SOCKET_HOST,
+  port: SOCKET_PORT
+};
+var socket = socketCluster.connect(options);
+socket.on('connect', function(){
+
+});
+
+socket.on('timeping', function(data){
+  timediff = (new Date()).getTime() - data;
+  console.log(timediff);
+});
+
+$('#test-play').click(function(event){
+  // var audio = new Audio('test.flac');
+  // audio.play();
+  socket.publish('play', timediff);
+});
+
+socket.subscribe('play').watch(function(time){
+  setTimeout(function(){
+    var audio = new Audio('test.flac');
+    audio.play();
+  }, 5000 - time - timediff);
+});
